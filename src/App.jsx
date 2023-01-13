5df8cc6488327514ecb2f98fc7ade2d4edc5c199
Dom van Almsick <dominicvana@gmail.com>
2023-01-13 18:56:46 +0000

    feat: basic header

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import githubIcon from './assets/icons_github-fill.svg';
+import feMentorIcon from './assets/icons_frontendmentor.svg';
+import linkedinIcon from './assets/icons_linkedin.svg';
+import twitterIcon from './assets/icons_twitter-fill.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p className="logo">domvanalmsick</p>
+        <ul className="icons">
+          <li className="icons github">
+            <a
+              href="https://github.com/dominicva"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={githubIcon} alt="GitHub icon" />
+            </a>
+          </li>
+          <li className="icons github">
+            <a
+              href="https://github.com/dominicva"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={feMentorIcon} alt="Frontend mentor icon" />
+            </a>
+          </li>
+          <li className="icons github">
+            <a
+              href="https://github.com/dominicva"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedinIcon} alt="LinkedIn icon" />
+            </a>
+          </li>
+          <li className="icons github">
+            <a
+              href="https://github.com/dominicva"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={twitterIcon} alt="Twitter icon" />
+            </a>
+          </li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
