55e132e72071971c6a45fb81f2df14de7b45b890
Dom van Almsick <dominicvana@gmail.com>
2023-01-15 12:23:50 +0000

    feat: skills desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import './App.css';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,31 @@
+function Skills() {
+  return (
+    <section className="skills">
+      <hr />
+      <ul>
+        <li>
+          <span className="skill">HTML</span> <span>4 Years Experience</span>
+        </li>
+        <li>
+          <span className="skill">CSS</span> <span>4 Years Experience</span>
+        </li>
+        <li>
+          <span className="skill">JavaScript</span>{' '}
+          <span>4 Years Experience</span>
+        </li>
+        <li>
+          <span className="skill">Accessibility</span>{' '}
+          <span>4 Years Experience</span>
+        </li>
+        <li>
+          <span className="skill">React</span> <span>3 Years Experience</span>
+        </li>
+        <li>
+          <span className="skill">Sass</span> <span>3 Years Experience</span>
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
